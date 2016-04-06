118a58d322bc1095c5d2f72c7c3bb4c1aaf6290a
Brock Reece <brockreece@gmail.com>
2016-04-06 17:36:21 +0100

    Use croudie picker

--- Src/CroudiePicker.vue
+++ Src/CroudiePicker.vue
@@ -1,3 +1,10 @@
+<style>
+    .ui.accordion.menu a.title.item {
+        padding: 0.92857143em 1.14285714em;
+        background: rgba(0, 0, 0, 0.05);
+    }
+</style>
+
 <template>
     <span>
         <div v-if="show_picker_button">
@@ -15,7 +22,7 @@
             <div class="ui basic segment">
                 <div class="ui top aligned two column grid">
                     <div class="column">
-                        <h2 class="ui header">Croudie Picker <span v-show="filteredCroudies.length > 1">({{filteredCroudies.length}} found)</span></h2>
+                        <h2 class="ui header">Croudie Picker <small v-show="filteredCroudies.length > 1">({{filteredCroudies.length}} found)</small></h2>
                     </div>
                     <div class="right floated right aligned column">
                         <a v-if="filteredCroudies.length > 1 && filteredCroudies.length < 50" transition="fade" class="ui blue basic button" @click="addAll">Select all {{ filteredCroudies.length }} croudies</a>
@@ -23,156 +30,83 @@
                     </div>
                 </div>
             </div>
-                <div class="ui stackable grid">
-                    <div class="ui five wide secondary column">
-                        <div class="ui basic segment">
-                            <div v-el:accordion class="ui vertical accordion fluid menu">
-                                <div class="item">
-                                    <div class="ui transparent icon input">
-                                        <input class="prompt" type="text" placeholder="Search croudies..." v-model="search" debounce="500">
-                                        <i class="search link icon"></i>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <div class="ui form">
-                                        <div class="grouped fields">
-                                            <div class="field">
-                                                <div class="ui radio checkbox">
-                                                    <input type="radio" v-model="croudie" value="">
-                                                    <label>All</label>
-                                                </div>
-                                            </div>
-                                            <div class="field">
-                                                <div class="ui radio checkbox">
-                                                    <input type="radio" v-model="croudie" value="1">
-                                                    <label>Super Croudies</label>
-                                                </div>
-                                            </div>
-                                            <div class="field">
-                                                <div class="ui radio checkbox">
-                                                    <input type="radio" v-model="croudie" value="0">
-                                                    <label>Croudies</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="item" v-if="croudie === '0'">
-                                    <div class="header">Hourly Rate <small>(<= £{{ rate }})</small></div>
-                                    <input style="width:100%" type="range" min="1" max="50" v-model="rate" debounce="500"/>
-                                </div>
-                                <input v-else value="0" v-model="rate" hidden/>
-                                <div class="item">
-                                    <a class="title">
-                                        <i class="dropdown icon"></i>
-                                        <div class="ui mini blue circular label">{{ language.length }}</div>
-                                        Languages
-                                        <small v-if="language.length" @click.stop="language = []">clear</small>
-                                    </a>
-                                    <div class="content">
-                                        <multi-search placeholder="Search languages..." :search="searchLanguage" :items.sync="language"></multi-search>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <a class="title">
-                                        <i class="dropdown icon"></i>
-                                        <div class="ui mini blue circular label">{{ country.length }}</div>
-                                        Countries
-                                        <small v-if="country.length" @click.stop="country = []">clear</small>
-                                    </a>
-                                    <div class="content">
-                                        <multi-search :flag="true" placeholder="Search countries..." :search="searchAddress" :items.sync="country"></multi-search>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <a class="title">
-                                        <i class="dropdown icon"></i>
-                                        <div class="ui mini blue circular label">{{ qualification.length }}</div>
-                                        Qualifications
-                                        <small v-if="qualification.length" @click.stop="qualification = []">clear</small>
-                                    </a>
-                                    <div class="content">
-                                        <multi-search placeholder="Search qualifications..." :search="searchQualification" :items.sync="qualification"></multi-search>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <a class="title">
-                                        <i class="dropdown icon"></i>
-                                        <div class="ui mini blue circular label">{{ availability.length }}</div>
-                                        Availability
-                                        <small v-if="availability.length" @click.stop="availability = []">clear</small>
-                                    </a>
-                                    <div class="content">
-                                        <div class="ui form">
-                                            <div class="grouped fields">
-                                                <div class="field" v-for="day in days">
-                                                    <div class="ui checkbox">
-                                                        <input type="checkbox" id="{{ day }}" value="{{ day }}" v-model="availability">
-                                                        <label for="{{ day }}">{{ day }}</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+            <div class="ui stackable grid">
+                <div class="ui five wide secondary column">
+                    <div class="ui basic segment">
+                        <div v-el:accordion class="ui vertical accordion fluid menu">
+                            <!-- <a class="active title item">
+                                <i class="dropdown icon"></i>
+                                Rules
+                            </a> -->
+                            <div class="active content">
+                                <croudie-picker-menu :croudie.sync="croudie" :language.sync="language" :country.sync="country" :qualification.sync="qualification" :availability.sync="availability" :rate.sync="rate" :search.sync="search"></croudie-picker-menu>
                             </div>
+
+                            <!-- <a class="title item">
+                                <i class="dropdown icon"></i>
+                                Filters
+                            </a>
+                            <div class="content">
+                                <filter-menu :rules.sync="filters"></filter-menu>
+                            </div> -->
                         </div>
                     </div>
+                </div>
 
-                    <div class="ui seven wide center aligned column">
-                        <div v-if="filteredCroudies.length === 0 && croudies.length > 0" class="ui very padded basic segment">
-                            <h4 class="ui icon grey header">
-                                <i class="circular users icon"></i>
-                                All croudies in this filter have been selected
-                            </h4>
+                <div class="ui seven wide center aligned column">
+                    <div v-if="filteredCroudies.length === 0 && croudies.length > 0" class="ui very padded basic segment">
+                        <h4 class="ui icon grey header">
+                            <i class="circular users icon"></i>
+                            All croudies in this filter have been selected
+                        </h4>
+                    </div>
+                    <div v-if="croudies.length === 0" class="ui very padded basic segment">
+                        <h4 class="ui icon grey header">
+                            <i class="circular users icon"></i>
+                            No croudies match this filter
+                        </h4>
+                    </div>
+                    <div :class="dimmerClasses">
+                        <div class="ui text large loader">Loading</div>
+                    </div>
+                    <div v-for="croudie in filteredCroudies | limitBy limit" @click="add(croudie)" class="ui fluid yellow card" >
+                        <div class="left aligned content">
+                            <img v-if="croudie.avatar" class="left floated mini circular ui image" :src="croudie.avatar" />
+
+                            <div class="header">{{ croudie.name }}</div>
+                            <div class="meta">{{ croudie.email }}</div>
                         </div>
-                        <div v-if="croudies.length === 0" class="ui very padded basic segment">
-                            <h4 class="ui icon grey header">
-                                <i class="circular users icon"></i>
-                                No croudies match this filter
-                            </h4>
-                        </div>
-                        <div :class="dimmerClasses">
-                            <div class="ui text large loader">Loading</div>
-                        </div>
-                        <div v-for="croudie in filteredCroudies | limitBy limit" @click="add(croudie)" class="ui fluid yellow card" >
-                            <div class="left aligned content">
+                    </div>
+                    <a v-if="filteredCroudies.length > limit" class="ui circular basic icon button" @click="loadMore">
+                        Show more
+                        <semantic-icon class="dropdown"></semantic-icon>
+                    </a>
+                </div>
+
+                <div class="ui four wide column">
+                    <div class="ui basic segment">
+                        <strong class="ui header">Selected Croudies <small v-if="selected.length" @click.stop="selected = []">clear</small></strong>
+                        <div class="ui relaxed list">
+                            <div class="item" v-for="croudie in selected" transition="fade">
                                 <img v-if="croudie.avatar" class="left floated mini circular ui image" :src="croudie.avatar" />
-
-                                <div class="header">{{ croudie.name }}</div>
-                                <div class="meta">{{ croudie.email }}</div>
-                            </div>
-                        </div>
-                        <a v-if="filteredCroudies.length > limit" class="ui circular basic icon button" @click="loadMore">
-                            Show more
-                            <semantic-icon class="dropdown"></semantic-icon>
-                        </a>
-                    </div>
-
-                    <div class="ui four wide column">
-                        <div class="ui basic segment">
-                            <strong class="ui header">Selected Croudies <small v-if="selected.length" @click.stop="selected = []">clear</small></strong>
-                            <div class="ui relaxed list">
-                                <div class="item" v-for="croudie in selected" transition="fade">
-                                    <img v-if="croudie.avatar" class="left floated mini circular ui image" :src="croudie.avatar" />
-                                    {{ croudie.name }}
-                                    <semantic-icon icon="close" colour="red" link @click="remove(croudie)"></semantic-icon>
-                                </div>
+                                {{ croudie.name }}
+                                <semantic-icon icon="close" colour="red" link @click="remove(croudie)"></semantic-icon>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </semantic-modal>
     </span>
 </template>
 
 <script>
     import Vue from 'vue'
+    import FilterMenu from './FilterMenu.vue'
+    import CroudiePickerMenu from './CroudiePickerMenu.vue'
 
     export default {
         props: {
-
             show_picker_button : {
                 default() {
                     return true
@@ -210,52 +144,30 @@
             },
             croudie: {
                 default() {
-                    return ''
+                    return null
                 },
             },
         },
 
         data() {
             return {
-                croudies : [],
+                croudies: [],
                 loading: true,
                 showModal: false,
                 search: '',
-                days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
                 rate: 15,
                 limit: 5,
-
-                searchAddress: {
-                    url: '/core/api/country/?search={query}',
-                    fields: {
-                        results: 'data',
-                        title: 'name',
-                        description : 'code',
-                    },
-                },
-
-                searchLanguage: {
-                    url: '/core/api/language/?search={query}',
-                    fields: {
-                        results: 'data',
-                        title: 'name',
-                        description : 'code',
-                    },
-                },
-
-                searchQualification: {
-                    url: '/core/api/qualification/?search={query}',
-                    fields: {
-                        results: 'data',
-                        title: 'name',
-                    },
-                },
             }
         },
 
         ready() {
             $(this.$els.accordion).accordion()
             $.fn['iCheck'] = function() {}
+        },
+
+        components: {
+            FilterMenu,
+            CroudiePickerMenu,
         },
 
         methods: {
@@ -286,10 +198,6 @@
                     this.add(croudie)
                 })
             },
-
-            saveFilter() {
-                this.$http.post('/core/api/filter', this.filterRules).then((response) => {})
-            },
         },
 
         computed: {
@@ -308,6 +216,7 @@
                     tags: this.tags,
                     system: this.croudie,
                     rate: this.rate,
+                    include: true,
                 }
 
                 this.loading = true
@@ -318,16 +227,6 @@
                 })
 
                 return data
-            },
-
-            filterRules() {
-                return {
-                    name: 'Test',
-                    content: 'Test Filter',
-                    rules: [
-                        this.filters,
-                    ],
-                }
             },
 
             dimmerClasses() {
@@ -342,13 +241,13 @@
 
         watch: {
             croudie(value) {
-                if (value === '0' && this.rate === '0') {
+                if (value === 0 && parseFloat(this.rate, 10) < 1) {
                     this.rate = 15
                 }
             },
         },
 
-        events : {
+        events: {
             'open-croudie-picker'() {
                 this.show()
             },
