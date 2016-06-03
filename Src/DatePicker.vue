5c3a10a97bedc906d7a3b021955bee29874d1d36
Paul Watkins <paul.watkins@croud.co.uk>
2016-06-03 10:34:04 +0100

    amends to date picker

--- Src/DatePicker.vue
+++ Src/DatePicker.vue
@@ -1,76 +1,76 @@
+
+
 <template>
-  <div>
+
+<div>
     <div class="ui action input">
-      <input v-el:pickerfield v-model="display" type="text" placeholder="{{placeholder}}">
-      <button v-el:pickerbutton class="ui icon button">
-        <i class="calendar outline icon"></i>
-      </button>
+        <input v-el:pickerfield v-model="display_date" type="text" placeholder="{{placeholder}}">
+        <button v-el:pickerbutton class="ui icon button">
+            <i class="calendar outline icon"></i>
+        </button>
     </div>
-  </div>
+</div>
+
 </template>
 
 <script>
 
-  import _ from 'underscore'
-  import Pikaday from 'pikaday'
-  import croudDate from 'croud-date-parser'
-  export default {
+import _ from 'underscore'
+import Pikaday from 'pikaday'
+import croudDate from 'croud-date-parser'
+export default {
     props: {
-      placeholder: {
-        default: 'Date..'
-      },
-      date: {
-        default: null
-      },
-      reference: {
-        default: null
-      },
-      display: {
-        default: 'ddd YYYY'
-      },
-      settings: {
-        type: Object,
-        default() {
-          return {}
+        placeholder: {
+            default: 'Date..'
+        },
+        date: {
+            default: null
+        },
+        reference: {
+            default: null
+        },
+        display: {
+            default: 'D MMM YYYY'
+        },
+        settings: {
+            type: Object,
+            default () {
+                return {}
+            }
         }
-      }
     },
     data() {
-      return {
-        display: null
-      }
+        return {
+            display_date: null
+        }
     },
     watch: {
-      display() {
-        let type = this.reference ? 'end' : 'start',
-            date = croudDate(this.date, moment(), 'start');
+      display_date() {
+        // let type = this.reference ? 'end' : 'start',
+        //     date = croudDate(this.date, moment(), 'start');
 
-        this.date = croudDate(this.date, this.reference, type);
+        this.date = moment(croudDate(this.display_date));
       }
     },
     ready() {
 
+        this.display_date = this.date ? this.date.format(this.display) : moment().format(this.display)
+
+        // console.log(display_date)
+
         let settings = {
-          field: this.$els.pickerfield,
-          trigger: this.$els.pickerbutton,
-          onSelect: (date) => {
-            this.model = date
-            // console.log(date);
-               // field.value = picker.toString();
-           }
-       }
+            format: this.display,
+            // defaultDate: this.display_date,
+            field: this.$els.pickerfield,
+            trigger: this.$els.pickerbutton,
+            onSelect: (date) => {
+                this.date = moment(date)
+            }
+        }
 
-          // new Pikaday(
-          // {
-          //     field: document.getElementById('datepicker'),
-          //     trigger: document.getElementById('datepicker-button'),
-          //     minDate: new Date(2000, 0, 1),
-          //     maxDate: new Date(2020, 12, 31),
-          //     yearRange: [2010,2020]
-          // });
-
-       new Pikaday(_.extend({}, this.settings, settings));
+        new Pikaday(_.extend({}, this.settings, settings));
 
     }
-  }
+}
+
 </script>
