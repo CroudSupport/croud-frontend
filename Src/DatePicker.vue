a7d94cac8d67ec22bd5bb0f7d9a5bc1a44bc813c
Paul Watkins <paul.watkins@croud.co.uk>
2016-07-07 10:44:45 +0100

    version bump

--- Src/DatePicker.vue
+++ Src/DatePicker.vue
@@ -1,16 +1,14 @@
 
 
 <template>
-
-<span>
-    <div class="ui action input">
-        <input v-el:pickerfield v-model="display_date" type="text" class="{{classname}}" placeholder="{{placeholder}}">
-        <button v-el:pickerbutton class="ui icon button">
-            <i class="{{icon}} icon"></i>
-        </button>
-    </div>
-</span>
-
+    <span>
+        <div class="ui action input">
+            <input v-el:pickerfield @click.prevent.enter="updateDate" value="{{display_date}}" type="text" placeholder="{{placeholder}}">
+            <button @click.prevent v-el:pickerbutton class="ui icon button">
+                <i class="calendar outline icon"></i>
+            </button>
+        </div>
+    </span>
 </template>
 
 <script>
@@ -26,55 +24,74 @@ export default {
         date: {
             default: null
         },
+        min_date: {
+            default: null
+        },
+        max_date: {
+            default: null
+        },
         reference: {
             default: null
         },
         display: {
             default: 'D MMM YYYY'
         },
-        classname: {
-            default: '',
-        },
         settings: {
             type: Object,
             default () {
                 return {}
             }
-        },
-        icon: {
-            default: 'calendar'
         }
     },
     data() {
         return {
-            display_date: null
+            display_date: null,
+            picker: null
         }
     },
     watch: {
-      display_date() {
-        // let type = this.reference ? 'end' : 'start',
-        //     date = croudDate(this.date, moment(), 'start');
+      date() {
+         if (this.date.toJSON() !== this.picker.getMoment().toJSON()) {
+            this.picker.setMoment(this.date)
+        }
+      },
+      min_date() {
+          if (!this.picker) return
+        //   this.picker.setMinDate(this.min_date)
+      },
+      max_date() {
+          if (!this.picker) return
+        //   this.picker.setMaxDate(this.max_date)
+      },
+    },
+    methods: {
+        updateDate() {
+            this.date = moment(croudDate(this.display_date))
+        },
+        create() {
+            const inst = this
+            if (this.picker) this.picker.destroy()
+            let settings = {
+                format: this.display,
+                field: this.$els.pickerfield,
+                trigger: this.$els.pickerbutton,
+                onSelect: (date) => {
+                    this.date = moment(date)
+                    this.display_date = this.date.format(this.display)
+                    if (this.settings.afterSelect && typeof this.settings.afterSelect == 'function')
+                        this.settings.afterSelect(this.date)
+                }
+            }
 
-        this.date = moment(croudDate(this.display_date));
-      }
+            this.picker = new Pikaday(_.extend(settings, this.settings))
+
+            if (this.date) this.picker.setMoment(this.date)
+        }
     },
     ready() {
-
         this.display_date = this.date ? this.date.format(this.display) : moment().format(this.display)
 
-        // console.log(display_date)
-
-        let settings = {
-            format: this.display,
-            field: this.$els.pickerfield,
-            trigger: this.$els.pickerbutton,
-            onSelect: (date) => {
-                this.date = moment(date)
-            }
-        }
-
-        new Pikaday(_.extend({}, this.settings, settings));
-
+        this.create()
     }
 }
 
